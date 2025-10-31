8d65381c98ed1d2252ac50466385b571f954fb29
Mahmoud Dahdouh <mahmooddahdooh99@gmail.com>
2025-10-31 09:30:28 +0200

    setup project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
